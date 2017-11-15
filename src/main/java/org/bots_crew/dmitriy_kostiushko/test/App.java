f83ff00a9de28f1443548abbb310e8faed9fc2c4
Dmitrij <dim.kost.94@gmail.com>
2017-11-16 00:36:42 +0200

    rolled back some changes, cyrilic in windows command line is not as easy as i thought =(

--- src/main/java/org/bots_crew/dmitriy_kostiushko/test/App.java
+++ src/main/java/org/bots_crew/dmitriy_kostiushko/test/App.java
@@ -40,7 +40,7 @@ public class App {
 
 
     public static void main(String[] args) throws IOException {
-        testConverString();
+        //testConverString();
         H2DatabaseConnector connector = new H2DatabaseConnector();
         BookShelfService bookShelfService = new BookShelfService(connector);
         UserCommandsController controller = new UserCommandsController(bookShelfService);
